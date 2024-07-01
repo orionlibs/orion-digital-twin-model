c0bf28207a4504959ff2d06d028f5f19f176ebaa
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-07-01 21:59:13 +0100

    implemented more fields

--- src/main/java/io/github/orionlibs/orion_digital_twin_model/entity/ThingModel.java
+++ src/main/java/io/github/orionlibs/orion_digital_twin_model/entity/ThingModel.java
@@ -1,6 +1,7 @@
 package io.github.orionlibs.orion_digital_twin_model.entity;
 
 import io.github.orionlibs.core.data.validation.annotation.WithRegEx;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +15,47 @@ import lombok.Setter;
 @Setter
 public class ThingModel
 {
+    //Unique identifier representing the thing
     @WithRegEx("(?<ns>|(?:(?:[a-zA-Z]\\w*+)(?:[.-][a-zA-Z]\\w*+)*+)):(?<name>[^\\x00-\\x1F\\x7F-\\xFF/]++)")
-    private String id;
+    private String thingID;
+    //Links to the ID of an existing Policy which contains the authorization information applied for this thing
+    @WithRegEx("(?<ns>|(?:(?:[a-zA-Z]\\w*+)(?:[.-][a-zA-Z]\\w*+)*+)):(?<name>[^\\x00-\\x1F\\x7F-\\xFF/]++)")
+    private String policyID;
+    //The definition of this thing declaring its model in the form '::' or a valid HTTP(S) URL
+    @WithRegEx(value = {"([_a-zA-Z0-9\\-.]+):([_a-zA-Z0-9\\-.]+):([_a-zA-Z0-9\\-.]+)",
+                    "^((http|https)?:\\/\\/)"
+                                    + "((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,}|"
+                                    + "((\\d{1,3}\\.){3}\\d{1,3}))"
+                                    + "(\\:\\d+)?(\\/[-a-zA-Z\\d%_.~+]*)*"
+                                    + "(\\?[;&a-zA-Z\\d%_.~+=-]*)?"
+                                    + "(\\#[-a-zA-Z\\d_]*)?$"})
+    private String definition;
+    //The Attributes that describe this thing in more detail. Can be an arbitrary JSON object. Attributes are typically used to model rather static properties at the thing level. Static means that the values do not change as frequently as property values of Features
+    private Object attributes;
+    //The Features belonging to this thing. A thing can handle any number of Features. The key of this object represents the featureId
+    private List<Feature> features;
+
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    @Setter
+    public static class Feature
+    {
+        private String featureID;
+        //The definition of the Feature declaring its model, a list of Identifiers containing at least 1 Identifier in the form '::' or a valid HTTP(s) URL
+        @WithRegEx(value = {"([_a-zA-Z0-9\\-.]+):([_a-zA-Z0-9\\-.]+):([_a-zA-Z0-9\\-.]+)",
+                        "^((http|https)?:\\/\\/)"
+                                        + "((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,}|"
+                                        + "((\\d{1,3}\\.){3}\\d{1,3}))"
+                                        + "(\\:\\d+)?(\\/[-a-zA-Z\\d%_.~+]*)*"
+                                        + "(\\?[;&a-zA-Z\\d%_.~+=-]*)?"
+                                        + "(\\#[-a-zA-Z\\d_]*)?$"})
+        private List<String> definition;
+        //The data related to a Feature is managed in form of a list of properties. Each property itself can be either a simple/scalar value or a complex object. Allowed is any JSON object
+        private Object properties;
+        //The desired data related to a Feature is managed in form of a list of desired properties. Each desired property itself can be either a simple/scalar value or a complex object. Allowed is any JSON object
+        private Object desiredProperties;
+    }
 }
--- src/test/java/io/github/orionlibs/orion_digital_twin_model/entity/ThingModelTest.java
+++ src/test/java/io/github/orionlibs/orion_digital_twin_model/entity/ThingModelTest.java
@@ -17,11 +17,13 @@ public class ThingModelTest extends ATest
     void test_id_invalid() throws Exception
     {
         ThingModel model = ThingModel.builder()
-                        .id("invalidID")
+                        .thingID("invalidID")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("test:test:test")
                         .build();
         InvalidFields invalidFields = ThingModelValidator.validate(model);
         assertEquals(1, invalidFields.getFields().size());
-        assertEquals(Set.of("id"), invalidFields.getFields());
+        assertEquals(Set.of("thingID"), invalidFields.getFields());
     }
 
 
@@ -29,32 +31,140 @@ public class ThingModelTest extends ATest
     void test_id_valid() throws Exception
     {
         ThingModel model = ThingModel.builder()
-                        .id("org.eclipse.ditto:smart-coffee-1")
+                        .thingID("org.eclipse.ditto:smart-coffee-1")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("test:test:test")
                         .build();
         InvalidFields invalidFields = ThingModelValidator.validate(model);
         assertEquals(0, invalidFields.getFields().size());
         model = ThingModel.builder()
-                        .id("foo:bar")
+                        .thingID("foo:bar")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("test:test:test")
                         .build();
         invalidFields = ThingModelValidator.validate(model);
         assertEquals(0, invalidFields.getFields().size());
         model = ThingModel.builder()
-                        .id("org.eclipse.ditto_42:smart-coffeee")
+                        .thingID("org.eclipse.ditto_42:smart-coffeee")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("test:test:test")
                         .build();
         invalidFields = ThingModelValidator.validate(model);
         assertEquals(0, invalidFields.getFields().size());
         model = ThingModel.builder()
-                        .id("com.some-domain.ditto-rocks:foobar")
+                        .thingID("com.some-domain.ditto-rocks:foobar")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("test:test:test")
                         .build();
         invalidFields = ThingModelValidator.validate(model);
         assertEquals(0, invalidFields.getFields().size());
         model = ThingModel.builder()
-                        .id("org.eclipse:admin-policy")
+                        .thingID("org.eclipse:admin-policy")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("test:test:test")
                         .build();
         invalidFields = ThingModelValidator.validate(model);
         assertEquals(0, invalidFields.getFields().size());
         model = ThingModel.builder()
-                        .id("org.eclipse:admin policy")
+                        .thingID("org.eclipse:admin policy")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("test:test:test")
+                        .build();
+        invalidFields = ThingModelValidator.validate(model);
+        assertEquals(0, invalidFields.getFields().size());
+    }
+
+
+    @Test
+    void test_policyID_invalid() throws Exception
+    {
+        ThingModel model = ThingModel.builder()
+                        .thingID("org.eclipse.ditto:smart-coffee-1")
+                        .policyID("invalidID")
+                        .definition("test:test:test")
+                        .build();
+        InvalidFields invalidFields = ThingModelValidator.validate(model);
+        assertEquals(1, invalidFields.getFields().size());
+        assertEquals(Set.of("policyID"), invalidFields.getFields());
+    }
+
+
+    @Test
+    void test_policyID_valid() throws Exception
+    {
+        ThingModel model = ThingModel.builder()
+                        .thingID("org.eclipse.ditto:smart-coffee-1")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("test:test:test")
+                        .build();
+        InvalidFields invalidFields = ThingModelValidator.validate(model);
+        assertEquals(0, invalidFields.getFields().size());
+        model = ThingModel.builder()
+                        .thingID("foo:bar")
+                        .policyID("foo:bar")
+                        .definition("test:test:test")
+                        .build();
+        invalidFields = ThingModelValidator.validate(model);
+        assertEquals(0, invalidFields.getFields().size());
+        model = ThingModel.builder()
+                        .thingID("org.eclipse.ditto_42:smart-coffeee")
+                        .policyID("org.eclipse.ditto_42:smart-coffeee")
+                        .definition("test:test:test")
+                        .build();
+        invalidFields = ThingModelValidator.validate(model);
+        assertEquals(0, invalidFields.getFields().size());
+        model = ThingModel.builder()
+                        .thingID("com.some-domain.ditto-rocks:foobar")
+                        .policyID("com.some-domain.ditto-rocks:foobar")
+                        .definition("test:test:test")
+                        .build();
+        invalidFields = ThingModelValidator.validate(model);
+        assertEquals(0, invalidFields.getFields().size());
+        model = ThingModel.builder()
+                        .thingID("org.eclipse:admin-policy")
+                        .policyID("org.eclipse:admin-policy")
+                        .definition("test:test:test")
+                        .build();
+        invalidFields = ThingModelValidator.validate(model);
+        assertEquals(0, invalidFields.getFields().size());
+        model = ThingModel.builder()
+                        .thingID("org.eclipse:admin policy")
+                        .policyID("org.eclipse:admin policy")
+                        .definition("test:test:test")
+                        .build();
+        invalidFields = ThingModelValidator.validate(model);
+        assertEquals(0, invalidFields.getFields().size());
+    }
+
+
+    @Test
+    void test_definition_invalid() throws Exception
+    {
+        ThingModel model = ThingModel.builder()
+                        .thingID("org.eclipse.ditto:smart-coffee-1")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("invalidID")
+                        .build();
+        InvalidFields invalidFields = ThingModelValidator.validate(model);
+        assertEquals(1, invalidFields.getFields().size());
+        assertEquals(Set.of("definition"), invalidFields.getFields());
+    }
+
+
+    @Test
+    void test_definition_valid() throws Exception
+    {
+        ThingModel model = ThingModel.builder()
+                        .thingID("org.eclipse.ditto:smart-coffee-1")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("test:test:test")
+                        .build();
+        InvalidFields invalidFields = ThingModelValidator.validate(model);
+        assertEquals(0, invalidFields.getFields().size());
+        model = ThingModel.builder()
+                        .thingID("org.eclipse.ditto:smart-coffee-1")
+                        .policyID("org.eclipse.ditto:smart-coffee-1")
+                        .definition("https://example.com/definition")
                         .build();
         invalidFields = ThingModelValidator.validate(model);
         assertEquals(0, invalidFields.getFields().size());
